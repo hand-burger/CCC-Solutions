491bae27556adcb0c9995d798a6d4e192d4612cd
hand-burger <jtmcash5@gmail.com>
2021-07-22 10:36:26 -0400

    🦑 More 1996

---
+++ CCC 1996/P4.py
@@ -0,0 +1,7 @@
+inn = '/Users/jack/Documents/CCC-Solutions/CCC 1996/rom.in'
+outt = '/Users/jack/Documents/CCC-Solutions/CCC 1996/rom.out'
+
+# Open input files
+fin = open(inn)
+fout = open(outt, 'w')
+
