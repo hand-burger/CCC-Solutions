ac99e2f946d7c38a2194f2f20fd041eefeb8df1d
hand-burger <jtmcash5@gmail.com>
2021-07-26 11:14:48 -0400

    🐍 Started 1997

---
+++ CCC 1997/P1.py
@@ -0,0 +1,34 @@
+inn = '/Users/jack/Documents/CCC-Solutions/CCC 1997/sent.in'
+outt = '/Users/jack/Documents/CCC-Solutions/CCC 1997/sent.out'
+
+# Open the files for reading and writing purposes
+fin = open(inn)
+fout = open(outt, 'w')
+
+firstLine = fin.readline().strip()
+
+# Repeat for each data set in the input file
+for i in range(0, int(firstLine)):
+    # Get the number of subjects, verbs and objects
+    subjects = fin.readline().strip()
+    verbs = fin.readline().strip()
+    objects = fin.readline().strip()
+
+    # Initialize arrays
+    subs = [None] * int(subjects)
+    vebs = [None] * int(verbs)
+    obs = [None] * int(objects)
+
+    # Load arrays
+    for i in range(0, int(subjects)):
+        subs[i] = fin.readline().strip()
+    for i in range(0, int(verbs)):
+        vebs[i] = fin.readline().strip()
+    for i in range(0, int(objects)):
+        obs[i] = fin.readline().strip()
+
+    # Simple triple loop
+    for j in range(0, int(subjects)):
+        for k in range(0, int(verbs)):
+            for m in range(0, int(objects)):
+                print(subs[j] + ' ' + vebs[k] + ' ' + obs[m] + '.')
