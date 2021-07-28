d87e124e0d9f1b4bd49e049178f93490f1d61912
hand-burger <jtmcash5@gmail.com>
2021-07-27 21:20:53 -0400

    🦋 Added 2021 Senior

--- CCC 2021/Senior/S1.py
+++ CCC 2021/Senior/S1.py
@@ -2,14 +2,14 @@
 
 n = int(input())
 
-# Put each length into the array
+# Put each length and width input into the array
 
 arr = list(map(int, input().split()))
 brr = list(map(int, input().split()))
 
 area = 0
 
-# Loop through each fence piece
+# Loop through each fence piece and get the area
 
 for i in range(1, n + 1):
     area += (arr[i] + arr[i - 1]) * brr[i - 1] / 2
