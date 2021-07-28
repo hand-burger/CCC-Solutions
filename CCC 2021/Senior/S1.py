1edcd6a9d8394db993f67782fd8012404f127782
hand-burger <jtmcash5@gmail.com>
2021-07-27 22:16:16 -0400

    🦋 Added 2021 Senior

--- CCC 2021/Senior/S1.py
+++ CCC 2021/Senior/S1.py
@@ -4,14 +4,16 @@ n = int(input())
 
 # Put each length and width input into the array
 
-arr = list(map(int, input().split()))
-brr = list(map(int, input().split()))
+lengths = list(map(int, input().split()))
+
+widths = list(map(int, input().split()))
 
 area = 0
 
 # Loop through each fence piece and get the area
 
 for i in range(1, n + 1):
-    area += (arr[i] + arr[i - 1]) * brr[i - 1] / 2
+    # Remember that the planks are not rectangles, but trapezoids, and thus use the area formula is different
+    area += (lengths[i] + lengths[i - 1]) * widths[i - 1] / 2
 
 print(area)
