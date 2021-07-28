ed7b7ea6bad81dea711c3e30b5be812c9409c418
hand-burger <jtmcash5@gmail.com>
2021-07-28 15:50:29 -0400

    🐜 More 1997

---
+++ CCC 1997/P3.py
@@ -0,0 +1,38 @@
+inn = '/Users/jack/Documents/CCC-Solutions/CCC 1997/dkc.in'
+outt = '/Users/jack/Documents/CCC-Solutions/CCC 1997/dkc.out'
+
+# Open files
+fin = open(inn)
+fout = open(outt, 'w')
+
+# Get first line (number of numbers)
+firstLine = fin.readline()
+
+for i in range(int(firstLine)):
+    # Number of teams in the competition for that test case.
+    n = fin.readline()
+    # One-loss, eliminated, & count vars
+    ol = 0
+    el = 0
+    count = 0
+
+# Determine the number of rounds, undefeated, one-loss & eliminated
+while True:
+    # Initial print of status
+    print('Round: ' + str(count) + ': ' + str(n) + ' undefeated, ' + str(ol) + ' one-loss, ' + str(el) + ' eliminated')
+    fout.write('Round: ' + str(count) + ': ' + str(n) + ' undefeated, ' + str(ol) + ' one-loss, ' + str(el) + ' eliminated\n')
+    # When number of teams is 0 and one loss is 1, break the loop because it is the last round
+    if n == 0 and ol == 1:
+        break
+    # On the round before the last set one-line to 2 and n to 0
+    if n == 1 and ol == 1:
+        ol = 2
+        n = 0
+    else:
+        el = el + ol // 2
+        ol = ol - (ol // 2) + (int(n) // 2)
+        n = int(n) - (int(n) // 2)
+    # Once gone through all, increase the round count
+    count += 1
+print('There are ' + str(count) + ' rounds.')
+fout.write('There are ' + str(count) + ' rounds.')
