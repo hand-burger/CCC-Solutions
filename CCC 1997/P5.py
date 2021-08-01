0718821e5b7c4ca7bb4777127d05d641e737e9c0
hand-burger <jtmcash5@gmail.com>
2021-08-01 11:56:33 -0400

    🐷 Nearly done 1997

---
+++ CCC 1997/P5.py
@@ -0,0 +1,11 @@
+inn = '/Users/jack/Documents/CCC-Solutions/CCC 1997/div.in'
+outt = '/Users/jack/Documents/CCC-Solutions/CCC 1997/div.out'
+
+# Open files
+fin = open(inn)
+fout = open(outt, 'w')
+
+
+
+fin.close()
+fout.close()
