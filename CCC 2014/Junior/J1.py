48692dbda8b7e45c040fc5599771a843b45ec16e
hand-burger <jtmcash5@gmail.com>
2021-08-21 19:38:20 -0400

    🪰 Some 2014

---
+++ CCC 2014/Junior/J1.py
@@ -0,0 +1,12 @@
+angle_1 = int(input())
+angle_2 = int(input())
+angle_3 = int(input())
+
+if angle_1 + angle_2 + angle_3 != 180:
+    print('Error')
+elif angle_1 == 60 and angle_2 == 60 and angle_3 == 60:
+    print('Equilateral')
+elif angle_1 == angle_2 or angle_1 == angle_3 or angle_2 == angle_3:
+    print('Isosceles')
+else:
+    print('Scalene')
