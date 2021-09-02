1d764cea5c0b1d95c3ff5dd2526e1c5eace6bb1f
hand-burger <jtmcash5@gmail.com>
2021-09-02 15:32:06 -0400

    🦔 Some 2009

---
+++ CCC 2009/Junior/J1.py
@@ -0,0 +1,8 @@
+digit_11 = int(input())
+digit_12 = int(input())
+digit_13 = int(input())
+
+ten_digit_sum = 9*1+7*3+8*1+0*3+9*1+2*3+1*1+4*3+1*1+8*3
+
+print('The 1-3-sum is', ten_digit_sum +
+      digit_11 * 1 + digit_12 * 3 + digit_13 * 1)
