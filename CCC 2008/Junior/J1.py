724cd75e2cddd854792b50802cbef28839287319
hand-burger <jtmcash5@gmail.com>
2021-09-02 15:36:21 -0400

    🦨 Some 2008

---
+++ CCC 2008/Junior/J1.py
@@ -0,0 +1,11 @@
+weight = float(input())
+height = float(input())
+
+BMI = weight / (height * height)
+
+if BMI > 25:
+    print('Overweight')
+elif BMI < 18.5:
+    print('Underweight')
+else:
+    print('Normal weight')
