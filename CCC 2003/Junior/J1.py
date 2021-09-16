3e126773cca59ceb08abc8eaff8a987a60ce3cb1
hand-burger <jtmcash5@gmail.com>
2021-09-15 21:17:57 -0400

    🦄 Some 2003

---
+++ CCC 2003/Junior/J1.py
@@ -0,0 +1,12 @@
+t = int(input())
+s = int(input())
+h = int(input())
+spaces = s * ' '
+
+for i in range(t):
+    print('*' + spaces + '*' + spaces + '*')
+
+print((3 + 2 * s) * '*')
+
+for i in range(h):
+    print((s + 1) * ' ' + '*')
